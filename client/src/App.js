47d79beb6936b16220c1fb25d56db346a6164a7b
Bhushankoli22 <bhushukgce22@gmail.com>
2022-05-30 13:58:17 +0530

    simple mern app in 60 minutes with get post api calls

---
+++ client/src/App.js
@@ -0,0 +1,80 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name,
+      age,
+      username,
+    }).then((response) => {
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          age,
+          username,
+        },
+      ]);
+    });
+    
+  };
+
+  return (
+    <div className="App">
+      <div className="usersDisplay">
+        {listOfUsers.map((user) => {
+          return (
+            <div>
+              <p>Name: {user.name}</p>
+              <p>Age: {user.age}</p>
+              <p>Username: {user.username}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          name="name"
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+          name="age"
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+          name="username"
+        />
+        <button onClick={createUser}> Create User </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
